6638caf35259d7a1f594bfb23a373ea89ee40161
malissab <malissambishop7@gmail.com>
2021-11-23 11:46:33 -0800

    done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
-# Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+    "Hello, programmer!"   
+end
+
+def greet(name = "Naureen")
+    puts "Hello, #{name}!"
+end
+greet
+greet "Jimmy"
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+def halve(num)
+    if num.class == Integer
+        return num/2
+    end
+    return nil
+end
+
+
